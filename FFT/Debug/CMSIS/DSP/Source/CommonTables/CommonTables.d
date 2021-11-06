e811d2eab58ccea313844bdef7f36068263a0dc3
Santi <56729478+SantiRaimondi@users.noreply.github.com>
2021-11-05 21:04:17 -0300

    Add files via upload

---
+++ FFT/Debug/CMSIS/DSP/Source/CommonTables/CommonTables.d
@@ -0,0 +1,21 @@
+CMSIS/DSP/Source/CommonTables/CommonTables.o \
+ CMSIS/DSP/Source/CommonTables/CommonTables.d: \
+ ../CMSIS/DSP/Source/CommonTables/CommonTables.c \
+ ../CMSIS/DSP/Source/CommonTables/arm_common_tables.c \
+ C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_math.h \
+ C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS/cmsis_compiler.h \
+ C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS/cmsis_gcc.h \
+ C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_common_tables.h \
+ C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_math.h \
+ ../CMSIS/DSP/Source/CommonTables/arm_const_structs.c \
+ C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_const_structs.h \
+ C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_common_tables.h
+../CMSIS/DSP/Source/CommonTables/arm_common_tables.c:
+C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_math.h:
+C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS/cmsis_compiler.h:
+C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS/cmsis_gcc.h:
+C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_common_tables.h:
+C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_math.h:
+../CMSIS/DSP/Source/CommonTables/arm_const_structs.c:
+C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_const_structs.h:
+C\:\Users\santi\Desktop\Datardos\Facultad\Segundo\ semestre\ Quinto\DSP\workspace\FFT\CMSIS\DSP\Include/arm_common_tables.h:
---
+++ FFT/Debug/device/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../device/system_MK64F12.c 
+
+OBJS += \
+./device/system_MK64F12.o 
+
+C_DEPS += \
+./device/system_MK64F12.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+device/%.o: ../device/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -DPRINTF_ADVANCED_ENABLE=1 -DCPU_MK64FN1M0VLL12 -DCPU_MK64FN1M0VLL12_cm4 -DFRDM_K64F -DFREEDOM -DMCUXPRESSO_SDK -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=1 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -DSDK_OS_BAREMETAL -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\utilities" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\drivers" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\component\uart" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\component\lists" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\CMSIS\DSP\Include" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\CMSIS" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\device" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\utilities" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\drivers" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\component\uart" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\component\lists" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\CMSIS" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\device" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\board" -I"C:\Users\santi\Desktop\Datardos\Facultad\Segundo semestre Quinto\DSP\workspace\FFT\source" -O0 -fno-common -g3 -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
